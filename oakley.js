12745f8586a2eb298e7c8f72aadb5157d31feb19
Oakley Hall <ohall@grassroots-tech.com>
2014-08-13 16:57:56 -0400

    Update oakley.js

--- oakley.js
+++ oakley.js
@@ -5,3 +5,13 @@ var radiansToDegrees = function( rads ){
 var degreesToRadians = function(degrees) {
     return degrees * Math.PI / 180;
 };
+
+
+
+
+queue.push( [ calculateResult, this, kase ] );
+
+queue.forEach(function(calc){
+    calc[0](calc[1],calc[2]);
+    queue.shift();
+})
